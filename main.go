00deae3b27ce6d15b428a5509b11211f6cbb9488
Mirek Šiřina <mirek@ovec.net>
2023-12-04 19:19:29 +0100

    Refactor

--- main.go
+++ main.go
@@ -5,17 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/Ovec/pwd_manager/crypt"
-	"github.com/Ovec/pwd_manager/password"
-	"github.com/Ovec/pwd_manager/random"
 	"github.com/Ovec/pwd_manager/terminal"
 )
 
 var filePath = "storage"
-var newKey = ""
-var state = ""
 var key = ""
 var salt = "some nice salt should be here"
 
@@ -49,92 +44,21 @@ func main() {
 			return
 		}
 
-		plaintext, err := crypt.DecryptAES(fileContent, []byte(key))
-		if err != nil {
-			fmt.Println("Error decrypting plaintext:", err)
-			return
-		}
-
-		err = json.Unmarshal(plaintext, &passwordPairs)
-		if err != nil {
-			fmt.Println("Wrong password")
-			os.Exit(0)
-		}
-
-	}
-
-	fmt.Println("Press (L) for list, (A) for Add, (Q) for Quit")
-	fmt.Println("---------------------")
-
-	for {
-		fmt.Print("-> ")
-		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
-
-		if strings.Compare("Q", text) == 0 {
-			fmt.Println("Bye, have a nice day")
-			os.Exit(0)
-		}
-
-		if strings.Compare("A", text) == 0 {
-			fmt.Println("Enter new key")
-			state = "A"
-		}
-
-		if strings.Compare("A", state) == 0 && strings.Compare("A", text) != 0 {
-			newKey = text
-			state = "B"
-		}
-
-		if strings.Compare("B", state) == 0 && strings.Compare("B", text) != 0 {
-			println(state)
-			newValue, err := password.Generate(random.RandomNumber(8, 16))
+		if len(fileContent) > 0 {
+			plaintext, err := crypt.DecryptAES(fileContent, []byte(key))
 			if err != nil {
-				fmt.Println("Error:", err)
+				fmt.Println("Error decrypting plaintext:", err)
 				return
 			}
 
-			_, ok := passwordPairs[newKey]
-
-			if ok {
-				fmt.Println("Already in, sorry bro")
-				fmt.Printf("Id: %s - %s\n", newKey, passwordPairs[newKey])
-			} else {
-				fmt.Println(newKey)
-				fmt.Println(newValue)
-
-				passwordPairs[newKey] = newValue
-
-				jsonData, err := json.Marshal(passwordPairs)
-				if err != nil {
-					fmt.Println("Error:", err)
-					return
-				}
-
-				cipherText, err := crypt.EncryptAES([]byte(jsonData), []byte(key))
-				if err != nil {
-					fmt.Println("Error encrypting plaintext:", err)
-					return
-				}
-
-				fmt.Println(string(cipherText))
-
-				err = os.WriteFile(filePath, cipherText, 0644)
-				if err != nil {
-					fmt.Println("Error:", err)
-					return
-				}
-
-			}
-
-			state = ""
-		}
-
-		if strings.Compare("L", text) == 0 {
-			println(state)
-			for key, value := range passwordPairs {
-				fmt.Printf("Id: %s - %s\n", key, value)
+			err = json.Unmarshal(plaintext, &passwordPairs)
+			if err != nil {
+				fmt.Println("Wrong password")
+				os.Exit(0)
 			}
 		}
+
 	}
+
+	terminal.Handle(reader, passwordPairs, filePath, key)
 }
--- terminal/terminal.go
+++ terminal/terminal.go
@@ -2,10 +2,17 @@ package terminal
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 	"syscall"
 
 	"golang.org/x/term"
+
+	"github.com/Ovec/pwd_manager/crypt"
+	"github.com/Ovec/pwd_manager/password"
+	"github.com/Ovec/pwd_manager/random"
 )
 
 func GetPassword(r *bufio.Reader) string {
@@ -17,4 +24,82 @@ func GetPassword(r *bufio.Reader) string {
 	}
 
 	return string(bytePassword)
+}
+
+func Handle(r *bufio.Reader, passwordPairs map[string]string, filePath string, key string) {
+	var newKey = ""
+	var state = ""
+	fmt.Println("Press (L) for list, (A) for Add, (Q) for Quit")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := r.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("Q", text) == 0 {
+			fmt.Println("Bye, have a nice day")
+			os.Exit(0)
+		}
+
+		if strings.Compare("A", text) == 0 {
+			fmt.Println("Enter new key")
+			state = "A"
+		}
+
+		if strings.Compare("A", state) == 0 && strings.Compare("A", text) != 0 {
+			newKey = text
+			state = "B"
+		}
+
+		if strings.Compare("B", state) == 0 && strings.Compare("B", text) != 0 {
+			println(state)
+			newValue, err := password.Generate(random.RandomNumber(8, 16))
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+
+			_, ok := passwordPairs[newKey]
+
+			if ok {
+				fmt.Println("Key exists already")
+				fmt.Printf("Id: %s - %s\n", newKey, passwordPairs[newKey])
+			} else {
+				fmt.Println(newKey)
+				fmt.Println(newValue)
+
+				passwordPairs[newKey] = newValue
+
+				jsonData, err := json.Marshal(passwordPairs)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+
+				cipherText, err := crypt.EncryptAES([]byte(jsonData), []byte(key))
+				if err != nil {
+					fmt.Println("Error encrypting plaintext:", err)
+					return
+				}
+
+				fmt.Println(string(cipherText))
+
+				err = os.WriteFile(filePath, cipherText, 0644)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+
+			}
+
+			state = ""
+		}
+
+		if strings.Compare("L", text) == 0 {
+			for key, value := range passwordPairs {
+				fmt.Printf("Id: %s - %s\n", key, value)
+			}
+		}
+	}
 }
