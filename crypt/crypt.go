87d31cf5661095c950776de0e1c8b903e05761f3
Mirek Šiřina <mirek@ovec.net>
2023-12-08 09:25:27 +0100

    Remove comments

--- crypt/crypt.go
+++ crypt/crypt.go
@@ -13,14 +13,12 @@ import (
 	"golang.org/x/crypto/pbkdf2"
 )
 
-// Encrypt plaintext with AES-256
 func EncryptAES(plaintext []byte, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
-	// Pad the plaintext if needed
 	plaintext = PKCS7Padding(plaintext, aes.BlockSize)
 
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
@@ -35,7 +33,6 @@ func EncryptAES(plaintext []byte, key []byte) ([]byte, error) {
 	return ciphertext, nil
 }
 
-// Decrypt ciphertext with AES-256
 func DecryptAES(ciphertext []byte, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -52,20 +49,17 @@ func DecryptAES(ciphertext []byte, key []byte) ([]byte, error) {
 	mode := cipher.NewCBCDecrypter(block, iv)
 	mode.CryptBlocks(ciphertext, ciphertext)
 
-	// Unpad the decrypted plaintext
 	ciphertext = PKCS7UnPadding(ciphertext)
 
 	return ciphertext, nil
 }
 
-// PKCS7Padding adds padding to the plaintext to make its length a multiple of blockSize
 func PKCS7Padding(plaintext []byte, blockSize int) []byte {
 	padding := blockSize - len(plaintext)%blockSize
 	padText := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(plaintext, padText...)
 }
 
-// PKCS7UnPadding removes padding from the plaintext
 func PKCS7UnPadding(plaintext []byte) []byte {
 	padding := int(plaintext[len(plaintext)-1])
 	return plaintext[:len(plaintext)-padding]
